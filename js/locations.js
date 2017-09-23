5291c8272cc51fe3928450b8523dfdb8b2cc3ba2
maulik <maulikdave9@gmail.com>
2017-09-23 13:08:10 -0400

    separate JSON data into a file.

---
+++ js/locations.js
@@ -0,0 +1,56 @@
+// Five Indian restaurants Locations in Buffalo, New York area.
+var locations = [{
+    title: "Namaste Indian Restaurant",
+    location: {
+      lat: 43.0012553,
+      lng: -78.7424039
+    },
+    url:     "http://www.namastebuffalo.com",
+    address: "224 Plaza Dr, Buffalo, NY 14221",
+    phone:   "(716) 636-0250",
+    fsId:    "57116d62498eb9387d5b261e"
+  },
+  {
+    title: "Taj Grill",
+    location: {
+      lat: 42.9461422,
+      lng: -78.8710662
+    },
+    url:     "http://www.tajgrillwny.com/",
+    address: "2290 Delaware Ave, Buffalo, NY 14216",
+    phone:   "(716) 875-1000",
+    fsId:    "4fbfb007e4b089f18b582fe8"
+  },
+  {
+    title: "Dosa Place",
+    location: {
+      lat: 42.9596609,
+      lng: -78.8207925
+    },
+    url:     "https://www.dosaplaceny.com/",
+    address: "3500 Main St, Buffalo, NY 14226",
+    phone:   "(716) 834-1400",
+    fsId:    "556b96e9498e15ee82f66e35"
+  },
+  {
+    title: "Taste of India",
+    location: {
+      lat: 42.9810856,
+      lng: -78.8174586
+    },
+    url:     "http://www.tasteofindia.com/",
+    address: "3192 Sheridan Dr, Buffalo, NY 14226",
+    phone:   "(716) 837-0460",
+    fsId:    "4b68b9faf964a5206e892be3"
+  },
+  {
+    title: "Spices of India",
+    location: {
+      lat: 42.9766988,
+      lng: -78.810341
+    },
+    url:     "http://www.spicesofindiawny.com/",
+    address: "438 Evans St, Williamsville, NY 14221",
+    phone:   "(716) 633-4800",
+    fsId:    "4ced486fb80da0930c052100"
+  }];
